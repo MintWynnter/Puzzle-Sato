edf04f46e9b92edbc943b2e336b3ef10e118a468
Sean <sosull@udel.edu>
2023-02-15 12:59:13 -0500

    started a board

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Tile } from './Tile'
+import { Tile } from './Tile';
+import { Player } from './Player';
 
 export function Board({size, coords}: {
     size: [number, number];
     coords: [number, number];
 }): JSX.Element {
-    return <div>
-        <Tile></Tile>
-
+    //state for the board
+    //change a tile to a player on movement
+    //probably want to set up a good board at some point
+    return <div style={{
+        display: 'grid',
+        gridTemplate: 'repeat(3, 3rem) / repeat(3, 3rem)',
+        gridGap: '0.3em',
+      }}>
     </div>;
 }
