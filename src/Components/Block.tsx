daa615c75d3587c6b35ad96f246000ec7f2323eb
Sean <sosull@udel.edu>
2023-02-24 13:48:38 -0500

    new branch

---
+++ src/Components/Block.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Player(): JSX.Element {
+    return <div style={ {backgroundColor: 'red', border: '1px solid black', padding: '4px'} }>
+        P
+    </div>;
+}
